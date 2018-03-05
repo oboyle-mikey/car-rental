412b0e94d97b4d26831feb988933c42850f87a79
Sean Berry <berryse@ITSERV.SCSS.TCD.IE>
2018-03-05 18:45:18 +0000

    Updated forms

--- Database Post/employeesPost.php
+++ Database Post/employeesPost.php
@@ -49,7 +49,7 @@ if($_SESSION['form_validation_err'] == 0){
 	$q  = "INSERT INTO employees (";
 	$q .= "office_ID, name, position, grade, base_salery, commission";
 	$q .= ") VALUES (";
-	$q .= "'$office_ID', '$name', '$position', '$grade', '$base_salery', '$commission'";
+	$q .= "'$office_ID', '$name', '$position', '$grade', '$base_salery', '$commission')";
 
 	$result = $db->query($q);
 
