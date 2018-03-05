f394078f9a9534abba53fde01967d973e9af80ad
conorwallace <conor-wallace@hotmail.co.uk>
2018-03-05 12:10:27 +0000

    Added Detail Form

---
+++ Data Enrty Forms/detail.php
@@ -0,0 +1,26 @@
+<?php
+
+$host = "localhost";
+
+$database = "st3001_1718_group_3db";
+
+$user = "group_3";
+
+$password = "vaeF9eip";
+
+
+
+
+@ $db = mysqli_connect($host, $user, $password, $database);
+
+$db->select_db($database);
+
+
+if (mysqli_connect_errno())
+{
+echo 'Error connecting to the db.';
+exit;
+}
+
+
+?>
