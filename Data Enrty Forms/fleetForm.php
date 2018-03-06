6cbcf8058e3998c8769f1e2bcc2e3b270d526a45
berryse <berryse@tcd.ie>
2018-03-06 16:11:15 +0000

    more form drop downs

--- Data Enrty Forms/fleetForm.php
+++ Data Enrty Forms/fleetForm.php
@@ -26,8 +26,25 @@
 <table style="width: 28%; height: 322px">
 	<tr>
 		<td style="width: 130px">Model</td>
-		<td style="width: 253px">
-			<input name="model" type="text" required/></td>
+				<td class="auto-style15" style="width: 261px">
+		<select name="EventID" style="width: 150px">
+			
+			
+			<?php 
+	include("detail.php");
+    $sql = "SELECT * FROM rates ORDER BY rate_ID desc";
+    $result = $db->query($sql);
+    
+    while($row = mysqli_fetch_assoc($result)) {
+        ?>
+
+					<option value="<?php echo $row['rate_ID'];?>"> 
+					</option>
+ 
+<?php
+}
+?>
+					</select></td>
 	</tr>
 	<tr>
 		<td style="width: 130px">Car Group name</td>
@@ -61,8 +78,25 @@
 	</tr>
 	<tr>
 		<td style="width: 130px">Office Location</td>
-		<td style="width: 253px">
-			<input name="office_ID" type="text" required/></td>
+				<td class="auto-style15" style="width: 261px">
+		<select name="EventID" style="width: 150px">
+			
+			
+			<?php 
+	include("detail.php");
+    $sql = "SELECT * FROM offices";
+    $result = $db->query($sql);
+    
+    while($row = mysqli_fetch_assoc($result)) {
+        ?>
+
+					<option value="<?php echo $row['office_ID'];?>"> <?php echo $row['address'];?> 
+					</option>
+ 
+<?php
+}
+?>
+					</select></td>
 	</tr>
 	
 	</table>
