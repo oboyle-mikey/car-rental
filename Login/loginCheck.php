25e2f41c47e02ecd2fa4e4a8979502d40f65baa8
conorwallace <conor-wallace@hotmail.co.uk>
2018-03-09 16:46:10 +0000

    Edit

--- Login/loginCheck.php
+++ Login/loginCheck.php
@@ -2,9 +2,49 @@
 
 session_start();
 
-$employee_ID = $_POST['employee_ID'];
-$_SESSION['access'] = $employee_ID;
 
-header('Location: Home.php');
+
+include ("detail.php");
+
+$employee_ID = $_POST['employee_ID'];
+$password = $_POST['password'];
+
+echo($employee_ID);
+echo($password);
+
+$_SESSION['employee_ID']= $employee_ID ;
+$_SESSION['position']="";
+$_SESSION['failedLogin'] = "";
+
+
+$sql = "select * from employees where employee_ID = '$employee_ID' and password = '$password'";
+$result = $db->querry($sql);
+
+$num_results = mysqli_num_rows ($result);
+$row = mysql_fetch_assoc($result);
+
+echo($sql);
+echo($num_results);
+
+//if($num_results==1)
+//{
+//    $sql = "select position from employees where employee_ID = '$employee_ID' ";
+//    $result = $db->querry($sql);
+//    $row = mysql_fetch_assoc($result);
+//    $_SESSION['position'] = $row['position'];
+//    echo($_SESSION['position']);
+   // header("Location: loginHome.php");
+//}
+
+//else
+//{
+
+
+//$_SESSION['failedLogin'] = "Login failed plese ensure you have entered the corect ID and password";
+//echo($_SESSION['failedLogin']);
+//header("Location:UnsuccessfulLogin");
+//}
+
+
 
 ?>
