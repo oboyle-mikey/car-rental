76c4a08ddc6c2139dc89915950728defd4e3e384
berryse <32709505+berryse@users.noreply.github.com>
2018-03-10 14:56:22 +0000

    This wasn't the final version from my branch, I kept the login stuff that was added

--- Database Post/payrollPost.php
+++ Database Post/payrollPost.php
@@ -9,48 +9,33 @@
 include ("detail.php"); 
 session_start();
 $_SESSION['form_validation_err'] = 0;
-
-
-if(empty($_POST['employee_ID'])){
+if(empty($_POST['pay'])){
 	$_SESSION['form_validation_err'] = 1;
 }else{
-	$employee_ID = test_input($_POST['employee_ID']);
+	$pay = test_input($_POST['pay']);
 }
-
-
+$pay_ID = $_SESSION['pay_ID'];
 //get office ID from employee ID
-$sql = "SELECT office_ID from employee WHERE employee_ID = '".$employee_ID."'";
+$sql = "SELECT office_ID from employees WHERE employee_ID = '".$pay_ID."'";
 		$result = $db->query($sql);
 		$office_ID = mysqli_fetch_assoc($result);
 		$office_ID = $office_ID['office_ID'];
-
-
-//calculates pay
-$sql = "SUM(price) from reservations where employee_ID = '".$employee_ID."' AND start_date between DATE_ADD(LAST_DAY(DATE_SUB(CURDATE(), interval 30 day)), interval 1 day) AND CURDATE())";
-		$result = $db->query($sql);
-		$office_ID = mysqli_fetch_assoc($result);
-		$office_ID = $office_ID['office_ID'];
-
-
+		
+//sets date
+$date = date("Y/m/d");		
 function test_input($data){
 	$data = trim($data);
 	$data = stripslashes($data);
 	$data = htmlspecialchars($data);
 	return $data;
 }
-
-
 if($_SESSION['form_validation_err'] == 0){
-
 	$q  = "INSERT INTO payroll (";
 	$q .= "employee_ID, office_ID, date, pay";
 	$q .= ") VALUES (";
-	$q .= "'$employee_ID', '$office_ID', '$date', '$pay')";
-
+	$q .= "'$pay_ID', '$office_ID', '$date', '$pay')";
 	$result = $db->query($q);
-
 }else{
 	header('Location: Home.php');
 }
-
 ?>
